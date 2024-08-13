6551af9a00be0bb5d1fadf66f74f89113628449a
Nicholas Codet <124813384+NicholasCodet@users.noreply.github.com>
2024-08-13 17:05:00 +0200

    Initial commit

---
+++ Dribbble/netlify/functions/dribbble.js
@@ -0,0 +1,21 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function (event, context) {
+  const DRIBBBLE_API_TOKEN = process.env.DRIBBBLE_API_TOKEN;
+
+  try {
+    const response = await fetch(
+      `https://api.dribbble.com/v2/user/shots?access_token=${DRIBBBLE_API_TOKEN}`
+    );
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "Failed to fetch data from Dribbble" }),
+    };
+  }
+};
